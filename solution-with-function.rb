b1c2a1fb92937382c0ab91be9323968c25f9dcd9
Dr. Mike Hopper <drmikeh@gmail.com>
2016-04-08 07:42:06 -0400

    Added comments.

--- solution-with-function.rb
+++ solution-with-function.rb
@@ -3,36 +3,41 @@ require 'io/console'
 def verify_password(password)
   violation_count = 0
 
+  # verify password has a length of 8 to 16 characters
   if password.length < 8
     violation_count += 1
     puts "Password is too short: does not meet length requirement of 8 to 16 characters"
   end
-
   if password.length > 16
     violation_count += 1
     puts "Password is too long: does not meet length requirement of 8 to 16 characters"
   end
 
+  # verify password contains at least 2 digits
   if password.count("0-9") < 2
     violation_count += 1
     puts "Password does not contain at least 2 digits"
   end
 
+  # verify password contains at least 2 lowercase letters
   if password.count("a-z") < 2
     violation_count += 1
     puts "Password does not contain at least 2 lowercase letters"
   end
 
+  # verify password contains at least 2 uppercase letters
   if password.count("A-Z") < 2
     violation_count += 1
     puts "Password does not contain at least 2 uppercase letters"
   end
 
+  # verify password contains at least 1 of the following _special_ characters: !, @, #, $, ^, &, ?, <, >
   if password.count("!, @, #, $, ^, &, ?, <, >") < 1
     violation_count += 1
     puts "Password does not contain at least 1 special character"
   end
 
+  # report results
   if (violation_count > 0)
     puts "Password has a total of #{violation_count} violations."
   else
